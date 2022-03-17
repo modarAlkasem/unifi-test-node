e5e5b6ea4df799bb5cd9f156ba1f477c7334b619
Modar Alkasem <modaralkasem@gmail.com>
2022-03-17 21:55:37 +0200

    Add Remove Todo feature

--- routes/todo.js
+++ routes/todo.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { body,param } = require('express-validator');
+const { body, param } = require('express-validator');
 
 
 const todoController = require('../controllers/todo');
@@ -7,28 +7,33 @@ const isAuth = require('../middlewares/is-auth');
 
 const router = express.Router();
 
-router.post('/todos',isAuth,[
+router.post('/todos', isAuth, [
     body('title').trim().notEmpty()
-    .withMessage("Title field should not be an empy")
-    .isLength({min : 5})
-    .withMessage('Description should be at least 5 characters'),
+        .withMessage("Title field should not be an empy")
+        .isLength({ min: 5 })
+        .withMessage('Description should be at least 5 characters'),
     body('description').trim().notEmpty()
-    .withMessage("Description field should not be an empy")
-    .isLength({min : 5})
-    .withMessage('Description should be at least 5 characters')
-],todoController.postTodo);
+        .withMessage("Description field should not be an empy")
+        .isLength({ min: 5 })
+        .withMessage('Description should be at least 5 characters')
+], todoController.postTodo);
 
-router.put('/todos/:todoId' , isAuth,[
+router.put('/todos/:todoId', isAuth, [
     param('todoId').trim().notEmpty()
-    .withMessage("Todo parameter should be provided!"),
+        .withMessage("Todo parameter should be provided!"),
     body('title').trim().notEmpty()
-    .withMessage("Title field should not be an empy")
-    .isLength({min : 5})
-    .withMessage('Description should be at least 5 characters'),
+        .withMessage("Title field should not be an empy")
+        .isLength({ min: 5 })
+        .withMessage('Description should be at least 5 characters'),
     body('description').trim().notEmpty()
-    .withMessage("Description field should not be an empy")
-    .isLength({min : 5})
-    .withMessage('Description should be at least 5 characters')
-] , todoController.updateTodo)
+        .withMessage("Description field should not be an empy")
+        .isLength({ min: 5 })
+        .withMessage('Description should be at least 5 characters')
+], todoController.updateTodo);
+
+router.delete('/todos/:todoId', isAuth, [
+    param('todoId').trim().notEmpty()
+        .withMessage("Todo parameter should be provided!")], todoController.deleteTodo);
+
 
 module.exports = router;
